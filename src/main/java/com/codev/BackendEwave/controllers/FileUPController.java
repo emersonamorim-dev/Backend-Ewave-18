32a2ca08c96e22ad197f646265a1d21d87d0a391
emersonamorim-dev <emerson_tecno@hotmail.com>
2023-09-11 13:30:26 -0300

    Aplicação Backend Eware - Java e SpringBoot 🚀 🔄 🌐

--- src/main/java/com/codev/BackendEwave/controllers/FileUPController.java
+++ src/main/java/com/codev/BackendEwave/controllers/FileUPController.java
@@ -12,20 +12,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class FileUPController {
 
     @Autowired
     private FileService fileService;  
     
 
-    @PostMapping(value = "/files/uploads", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping("/files/uploads")
     public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) {
         String fileName = file.getOriginalFilename();
         try {
